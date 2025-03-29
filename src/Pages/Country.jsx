9faaa649d4322759cefc260886d6168b2b100baf
Fred Robins <fredrobins619@outlook.com>
2025-03-29 22:51:39 +0000

    Extract code into smaller component & refactor

--- src/Pages/Country.jsx
+++ src/Pages/Country.jsx
@@ -1,8 +1,12 @@
 import { useParams } from "react-router";
 import useSWR from "swr";
 import BorderButtons from "../Components/BorderButtons";
+import Navbar from "../Components/Navbar"
 import leftArrow from "../../left-arrow-svgrepo-com.svg"
-import { useNavigate } from "react-router";
+import BackButton from "../Components/BackButton";
+import CountryDetail from "../Components/CountryDetail";
+import Loading from "../Components/Loading";
+import Unable from "../Components/Unable";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -21,69 +25,38 @@ function useCountry(countryName) {
 }
 
 export default function Country() {
-    let navigate = useNavigate();
     let { countryName } = useParams();
 
     const { user, isLoading, isError } = useCountry(countryName);
-    if (isLoading) return <p>Loading...</p>;
-    if (isError) return <p>Unable to retrieve data</p>;
+    if (isLoading) return <Loading />;
+    if (isError) return <Unable />;
     console.log(user) 
     const countries = Array.isArray(user) ? user : [user];
     // convert the object into an array because the first call return an array while the second one
     // returns an object 
 
     return (
-        <>
-            
+        <div className="min-h-[100vh] bg-l-light-gray">
+            <Navbar />
             {user.status == 404 ? <h1>The requested country doesn't exist :)</h1>
             :
             countries.map(country => (
-                <div key={country.name.common}>
-                    <button onClick={() => navigate(-1)}>
-                        {/* <img src={leftArrow} alt="leftArrow" /> */}
-                        Back
-                    </button> <br />
-
-                    <img src={country.flags.svg} alt={country.flags.alt} width={300} />
-                    <ul>
-                        <h1>{country.name.common}</h1>
-                        <li><strong>Native Name: </strong>
-                            {Object.values(country.name.nativeName)[0].common}
-                        </li>
-                        <li>
-                            <strong>Population: </strong>
-                            {country.population.toLocaleString()}
-                        </li>
-                        <li>
-                            <strong>Region: </strong>
-                            {country.region}
-                        </li>
-                        <li>
-                            <strong>Sub Region: </strong>
-                            {country.subregion}
-                        </li>
-                        <li>
-                            <strong>Capital: </strong>
-                            {country.capital + ''} {/* for countries like South Africa which have more than 1 capital */}
-                        </li>
-                        <br />
-                        <li>
-                            <strong>Top Level Domain: </strong>
-                            {country.tld}
-                        </li>
-                        <li>
-                            <strong>Currencies: </strong>
-                            {Object.values(country.currencies)[0].name}
-                        </li>
-                        <li>
-                            <strong>Languages: </strong>
-                            {Object.values(country.languages)[0]}
-                        </li>
-                    </ul>
-                    <p><strong>Border countries:</strong></p>
-                    <BorderButtons borders={country.borders} />
-                </div>
+                <CountryDetail 
+                    key={country.name.common}
+                    flagSrc={country.flags.svg}
+                    flagAlt={country.flags.alt}
+                    countryName={country.name.common}
+                    nativeName={Object.values(country.name.nativeName)[0]?.common}
+                    population={country.population}
+                    region={country.region}
+                    subregion={country.subregion}
+                    capital={country.capital}
+                    tld={country.tld}
+                    currencies={Object.values(country.currencies)[0]?.name}
+                    languages={Object.values(country.languages)[0]}
+                    borders={country.borders}
+                />
             ))}
-        </>
+        </div>
     )
 }
