5059fc4b7d647259b5307509712a35142aec4718
Fred Robins <fredrobins619@outlook.com>
2025-03-31 22:32:36 +0000

    Fix CSS syntax that caused major bug

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -2,7 +2,7 @@ import { FaSearch } from "react-icons/fa";
 
 export default function Input({ type, placeholder, onChange}) {
     return (
-        <div className="flex justify-center items-center gap-2 p-3 bg-white shadow w-full lg:max-w-80 rounded-lg mb-9 border border-gray-50 lg:ml-14 dark:bg-d-dark-blue dark:border-slate-800 placeholder:dark:text-white">
+        <div className="flex justify-center items-center gap-2 p-3 bg-white shadow w-full lg:max-w-80 rounded-lg mb-9 border border-gray-50 lg:ml-14 dark:bg-d-dark-blue dark:border-slate-800 dark:placeholder:text-white">
             <FaSearch />   
             <input 
                 type={type} 
